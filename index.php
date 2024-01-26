cd2e5635f788069700a22e57bc06a0e8c46a676c
alex02r <alex.giochi02@gmail.com>
2024-01-26 15:53:00 +0100

    [UPGRADE] Migrazione della funzione nel file function.php

--- index.php
+++ index.php
@@ -1,38 +1,5 @@
 <?php
-//funzione che genera password casuale
-    function generatePassword($leng){
-        $string1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $number = '1234567890';
-        $simbol = '!@-_?}{>]<[';
-        $password = '';
-        for ($i=0; $i < $leng; $i++) { 
-            if ($i <= 4) {
-                $num = $i;
-            }else{
-                $num = rand(0,3);
-            }
-            switch ($num) {
-                case 0:
-                    $password .= $string1[rand(0, strlen($string1) - 1 )];
-                    break;
-                case 1:
-                    $password .= $number[rand(0, strlen($number) - 1)];
-                    break;
-                case 2:
-                    $password .= strtolower($string1[rand(0, strlen($string1) - 1)]);
-                    break;
-                
-                case 3:
-                    $password .= $simbol[rand(0, strlen($simbol) - 1)];
-                    break;
-                    
-                default:
-                    $password .= strtolower($string1[rand(1, strlen($string1) - 1)]);
-                    break;
-            }
-        }
-        return str_shuffle($password) ;
-    }
+    include __DIR__.'/partials/functions.php';
 //controllo se il valore è stato inserito, se sia un numero e sia maggiore di 1 
     if(isset($_GET['length']) && is_numeric($_GET['length']) && $_GET['length'] >= 4){
         $psw = generatePassword((int)$_GET['length']);
--- partials/functions.php
+++ partials/functions.php
@@ -1,3 +1,36 @@
 <?php  
-    
+    //funzione che genera password casuale
+    function generatePassword($leng){
+        $string1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $number = '1234567890';
+        $simbol = '!@-_?}{>]<[';
+        $password = '';
+        for ($i=0; $i < $leng; $i++) { 
+            if ($i <= 4) {
+                $num = $i;
+            }else{
+                $num = rand(0,3);
+            }
+            switch ($num) {
+                case 0:
+                    $password .= $string1[rand(0, strlen($string1) - 1 )];
+                    break;
+                case 1:
+                    $password .= $number[rand(0, strlen($number) - 1)];
+                    break;
+                case 2:
+                    $password .= strtolower($string1[rand(0, strlen($string1) - 1)]);
+                    break;
+                
+                case 3:
+                    $password .= $simbol[rand(0, strlen($simbol) - 1)];
+                    break;
+                    
+                default:
+                    $password .= strtolower($string1[rand(1, strlen($string1) - 1)]);
+                    break;
+            }
+        }
+        return str_shuffle($password) ;
+    }
 ?>
