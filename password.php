ae772a131a6d9a8981900fbdc5018a7d8e1c234a
alex02r <alex.giochi02@gmail.com>
2024-01-26 15:56:25 +0100

    [UPGRADE] Inizializzazione pagina di reindirizzamento

---
+++ password.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password</title>
+</head>
+<body>
+    <header class="bg-dark text-white p-4">
+        <h1>Your password</h1>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-6">
+                    
+                </div>  
+            </div>
+        </div>
+    </main>
+</body>
+</html>
